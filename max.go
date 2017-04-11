773ce5373aa72f260373b1ccbf36d57d5afe6d91
Iman S S <bxcodec@gmail.com>
2017-04-11 17:44:50 +0700

    update max function

--- max.go
+++ max.go
@@ -3,7 +3,7 @@ package integres
 // Max return the maximun  value beetwen  arg and arg0.
 //
 // 	Example:
-//		x:=Min(5,6) //  x = 5
+//		x:=Min(5,6) //  x = 6
 //		y:=Min(5,5) //  y = 5
 func Max(arg int , arg0 int ) int {
 	
