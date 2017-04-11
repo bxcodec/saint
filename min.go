75a945a242e41bc6b485df20543f41efae45104c
Iman S S <bxcodec@gmail.com>
2017-04-11 17:19:02 +0700

    add documentation

--- min.go
+++ min.go
@@ -3,8 +3,8 @@ package integres
 // Min return the minimun  value beetwen  arg and arg0.
 //
 // 	Example:
-//		Min(5,6) => 5
-//		Min(5,5) => 5
+//		x:=Min(5,6) //  x = 5
+//		y:=Min(5,5) //  y = 5
 func Min(arg int , arg0 int ) int {
 	
 	if arg <=arg0{
