6c559c198a57558d9002e1c2514122c5c803fb9b
Iman S S <bxcodec@gmail.com>
2017-04-11 17:33:32 +0700

    add power function

---
+++ pow.go
@@ -0,0 +1,24 @@
+package integres
+
+// Pow returns the power  value of arg to arg0.
+//
+// Example : 
+//  x:=Pow(2,3) // 8
+//  x:=Pow(3,3) // 27
+
+
+func Pow(arg int , arg0 int) int {
+	if arg0 < 0 {
+		return 0
+	}
+	res := 1
+	for arg0 != 0 {
+		if (arg0 & 1) == 1 {
+			res *= arg
+		}
+		arg0 >>= 1
+		arg *= arg
+	}
+
+	return res
+}
