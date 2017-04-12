8fd6c10ee89d909a0549d8f83d156b23df8a0631
Iman S S <bxcodec@gmail.com>
2017-04-12 12:52:45 +0700

    update docs

--- abs.go
+++ abs.go
@@ -1,8 +1,8 @@
 // This library only provide some of function that "math" library doesn't support especially in integer's operation (int data type) .
 // Intregres also provide some function to ease in some of operations.
 //
-// Warning!!
-//
+// 
+//	 Warning!!		
 // 		Used only with int data type. 
 package intregers
 
